fb36619df195933b97a22db49b8ff6cd836ecfcb
abalex-ui <kibgheeo775533@gmail.com>
2026-02-21 15:26:37 +0500

    removed and reinstalled the compatible softwares for db connection

--- backend/authproject/Program.cs
+++ backend/authproject/Program.cs
@@ -1,3 +1,6 @@
+using authproject.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Adding the connction string 
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
