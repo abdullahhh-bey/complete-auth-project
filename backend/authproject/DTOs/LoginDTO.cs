15d1d078f4f4c8700d49619b6f622143a4275eb6
abalex-ui <kibgheeo775533@gmail.com>
2026-02-21 15:47:29 +0500

    some minor changes

--- backend/authproject/DTOs/LoginDTO.cs
+++ backend/authproject/DTOs/LoginDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace authproject.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace authproject.DTOs
 {
     public class LoginDTO
     {
+        [Required]
+        [EmailAddress(ErrorMessage = "Must be a valid email address.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters.")]
+        public string Password { get; set; } = string.Empty;
     }
 }
