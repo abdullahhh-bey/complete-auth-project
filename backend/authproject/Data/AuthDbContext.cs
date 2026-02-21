6460a50ca7e5e48db5938097b70d020ddccdf7ce
abalex-ui <kibgheeo775533@gmail.com>
2026-02-21 15:08:21 +0500

    installed the req softwares and created dbcontext

---
+++ backend/authproject/Data/AuthDbContext.cs
@@ -0,0 +1,16 @@
+﻿using authproject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace authproject.Data
+{
+    public class AuthDbContext : DbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+        //I will be having "consumers" as table for model "Users" in DB
+        public DbSet<User> Consumers { get; set; }
+        
+    }
+}
