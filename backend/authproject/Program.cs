3dce87a07603fd21742c4ed3844e0743aea617ce
abalex-ui <kibgheeo775533@gmail.com>
2026-03-02 01:56:24 +0500

    made branch

--- backend/authproject/Program.cs
+++ backend/authproject/Program.cs
@@ -13,7 +13,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR(); //Added the SignalR service in IOC container
